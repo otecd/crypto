8a4aa1d7671774343f76d1bb81a6a12ffc992f5d
Dmitriy <idimzz@gmail.com>
2019-10-14 10:24:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Crypto library
+
+Библиотека для операций шифрования и кодирования
+
+## Методы
+
+  - checkSignature,
+  - hashHmacWithBase64,
+  - encode,
+  - decode,
+  - sha1,
+  - md5,
+  - encrypt,
+  - decrypt
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import fs from 'fs'
+import crypto from 'crypto'
+import base64 from 'base-64'
+import cryptoJs from 'crypto-js'
+
+export const checkSignature = (data, sig, keyPath) => {
+  const key = fs.readFileSync(keyPath)
+
+  return crypto.createVerify('SHA1')
+    .update(data)
+    .verify(key, sig, 'base64')
+}
+export const encode = v => base64.encode(v)
+export const decode = v => base64.decode(v)
+export const hashHmacWithBase64 = (algo = 'sha256', v, k) => crypto.createHmac(algo, k)
+  .update(v)
+  .digest()
+  .toString('base64')
+export const sha1 = v => crypto.createHash('sha1')
+  .update(v, 'binary')
+  .digest('hex')
+export const md5 = v => crypto.createHash('md5')
+  .update(typeof v === 'string' ? v : JSON.stringify(v))
+  .digest('hex')
+export const encrypt = (v, salt) => cryptoJs.AES.encrypt(typeof v === 'string' ? v : JSON.stringify(v), salt)
+  .toString()
+export const decrypt = (v, salt) => JSON.parse(cryptoJs.AES.decrypt(v, salt)
+  .toString(cryptoJs.enc.Utf8))
+
+export default {
+  checkSignature,
+  hashHmacWithBase64,
+  encode,
+  decode,
+  sha1,
+  md5,
+  encrypt,
+  decrypt
+}
